1e8db701cfbe31a1c88e95051995834d759057a1
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-18 19:05:10 +0000

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 define('ROOT_PATH', __DIR__ . '/');
 
 // Base URL (for redirects)
-define('BASE_URL', 'http://169.239.251.102:341/~chidima.ugwu/EasySalles/');
+define('BASE_URL', 'http://169.239.251.102:341/~chidima.ugwu/EasySales/');
 
 // Start session safely
 if (session_status() === PHP_SESSION_NONE) {
